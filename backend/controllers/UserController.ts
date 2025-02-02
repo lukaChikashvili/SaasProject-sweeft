5fd2c343570215908b2ed8375934a9d4a5e3ad63
luka chikashvili <lukachikashvili2020@gmail.com>
2025-02-02 16:20:56 +0400

    add company detail update feature

--- backend/controllers/UserController.ts
+++ backend/controllers/UserController.ts
@@ -1,25 +1,67 @@
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
-import prisma from "../db/db.config";
-import { authenticate } from "../middleware/authenticate";
+import prisma from "../db/db.config.js";
+
+import vine, { errors } from "@vinejs/vine";
+import { updateUserSchema } from "../validations/authValidation.js";
 
 class UserController {
     static async updateUserDetails(req: Request, res: Response) {
         try {
-            
-            const { id } = req.user;
-            const { name, email, country, password, industry} = req.body;
+            const validator = vine.compile(updateUserSchema);
+            const payload = await validator.validate(req.body);
+
+            const { id } = req.user!;
+            const { name, email, country, password, industry} = payload;
 
             // password validation
-            
+            if(password) {
+                const salt = bcrypt.genSaltSync(10);
+                const hashedPassword = bcrypt.hashSync(password, salt);
+
+                const updatedUser = await prisma.company.update({
+                    where: { id },
+                    data: {
+                        name,
+                        email,
+                        country,
+                        password: hashedPassword,
+                        industry
+                    }
+                });
+
+                return res.status(200).json({ message: "User details updated successfully", user: updatedUser });
+
+
+            }
+
+            const updatedUser = await prisma.company.update({
+                where: { id },
+                data: {
+                    name,
+                    email,
+                    country,
+                    industry
+                }
+            });
+
+            return res.status(200).json({ message: "User details updated successfully", user: updatedUser });
+
 
 
         } catch (error) {
-            
+            if (errors) {
+                return res.status(400).json({ errors: errors });
+            }
+
+       
+            console.error(error);
+            return res.status(500).json({ message: "An error occurred while updating user details." });
+        }
         }
     }
 
-}
+
 
 
 
--- backend/routes/api.ts
+++ backend/routes/api.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController.js"
+import { authenticate } from "../middlewares/authMiddleware.js";
+import UserController from "../controllers/UserController.js";
 
 const router = Router();
 
 router.post('/auth/register', AuthController.register);
 router.post('/auth/login', AuthController.login);
 router.get("/auth/activate/:token", AuthController.activateAccount);
+
+// update company details
+router.put("/auth/update", authenticate, UserController.updateUserDetails);
 
 export default router;
